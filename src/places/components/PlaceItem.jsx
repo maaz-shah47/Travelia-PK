3c9c248bb4c25aa00f119e82a728a8a548b6f778
Maaz Shah <dev@192.168.10.9>
2022-09-12 01:35:41 +0500

    added custom button component

--- src/places/components/PlaceItem.jsx
+++ src/places/components/PlaceItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from '../../shared/components/UIElements/Card'
+import Button from '../../shared/components/UIElements/FormElements/Button/Button'
 
 import './PlaceItem.scss'
 
@@ -16,9 +17,9 @@ const PlaceItem = ({id, title, description, address, creatorId, coordinates, ima
           <p>{description}</p>
         </div>
         <div className='place-item__actions'>
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
+          <Button inverse>VIEW ON MAP</Button>
+          <Button to={`/places/${id}`} >EDIT</Button>
+          <Button danger>DELETE</Button>
         </div>
       </Card>
     </li>
