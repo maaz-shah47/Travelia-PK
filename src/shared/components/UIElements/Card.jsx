7562880b07ee2fa8d2b0e0331797c77131898feb
Maaz Shah <dev@192.168.10.9>
2022-09-11 16:01:31 +0500

    user module, card and avatar component

---
+++ src/shared/components/UIElements/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Card.scss'
+
+const Card = ({children, className, style}) => {
+  return (
+    <div className={`card ${className}`} style={style} >{children}</div>
+  )
+}
+
+export default Card
--- src/users/pages/Users.jsx
+++ src/users/pages/Users.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import Card from '../../shared/components/UIElements/Card'
+import UsersList from '../components/UsersList'
 
 const Users = () => {
+  const USERS = [
+    {
+      id: 1,
+      name: "Max Schwarz",
+      image: "https://images.unsplash.com/photo-1662553172331-10b51cdfa963?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2058&q=80",
+      places: 1
+    },
+    {
+      id: 2,
+      name: "Max",
+      image: "https://images.unsplash.com/photo-1662553172331-10b51cdfa963?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2058&q=80",
+      places: 2
+    },
+    {
+      id: 3,
+      name: "John",
+      image: "https://images.unsplash.com/photo-1662553172331-10b51cdfa963?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2058&q=80",
+      places: 5
+    }
+  ]
   return (
-    <div>Users Page!</div>
-  )
+    <>
+      <UsersList items={USERS} />
+    </>
+    )
 }
 
 export default Users
